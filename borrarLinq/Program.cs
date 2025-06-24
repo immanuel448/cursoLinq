200b4d4d007f704c3372faac6ac89e455c1191f9
immanuel448 <emmanuelmendoza448@gmail.com>
2025-06-24 09:12:05 -0600

    001 (where, select, orderBy)

--- borrarLinq/Program.cs
+++ borrarLinq/Program.cs
@@ -19,75 +19,45 @@ namespace borrarLinq
             {
                 new Estudiantes{ Id = 1, Nombre = "Nadia", Edad = 19, Carrera ="Medicina" },
                 new Estudiantes{ Id = 2, Nombre = "Iker", Edad = 20, Carrera ="Ingeniería" },
-                new Estudiantes{ Id = 3, Nombre = "Armando", Edad = 18, Carrera ="Abogacía" },
+                new Estudiantes{ Id = 3, Nombre = "Armando", Edad = 18, Carrera ="Derecho" },
                 new Estudiantes{ Id = 4, Nombre = "Erika", Edad = 22, Carrera ="Arquitectura" },
                 new Estudiantes{ Id = 5, Nombre = "Oldair", Edad = 20, Carrera ="Ingeniería" }
             };
 
             //LINQ-----------------------
-            //where
-            Console.WriteLine("\nSELECCIONE LA CARRERA PARA BUSCAR:");
-            string carreraElegida = Console.ReadLine();
-            var estudiantesDeLaCarrera = estudiantes
-                .Where(e => string.Equals(e.Carrera, carreraElegida, StringComparison.OrdinalIgnoreCase));
-
-            if (estudiantesDeLaCarrera.Any())
+            //where, filtrar datos
+            var estudiantesIngenieria =
+                estudiantes.Where(e => e.Carrera == "ingeniería");
+            Console.WriteLine($"\nLos estudiantes que estudian ingeniería son:");
+            foreach (var item in estudiantesIngenieria)
             {
-                //se encontraron resultados
-                Console.WriteLine($"Los estudiantes que estudian {carreraElegida} son:");
-                foreach (var item in estudiantesDeLaCarrera)
-                {
-                    Console.WriteLine($"{item.Nombre} con {item.Edad} años.");
-                }
+                Console.WriteLine($"-{item.Nombre} con {item.Edad} años.");
             }
-            else
-            {
-                Console.WriteLine($"No hay estudiantes con la carrera de {carreraElegida}");
-            }
-
-            //select
-            var soloNombres = estudiantes
-                .OrderBy(e => e.Nombre)
-                .Select(e => e.Nombre);
+            
+            //select, elegir los datos para mostrar
+            var soloNombres = estudiantes.Select(e => e.Nombre);
             Console.WriteLine($"\nSolo nombres de los estudiantes:");
-            foreach (var item in soloNombres)
+            foreach (var nombre in soloNombres)
             {
-                Console.WriteLine($"-{item}.");
+                Console.WriteLine($"-{nombre}.");
             }
 
-            //orderBy
-            string eleccion = "";
-            while (eleccion != "1" && eleccion != "2")
+            //orderBy, ordenamiento lógico (orden ascendente)
+            var ordenarEdadAscendente = estudiantes.OrderBy(e => e.Edad);
+            Console.WriteLine($"\nOrdenar estudiantes por edad ascendente:");
+            foreach (var estudiante in ordenarEdadAscendente)
             {
-                Console.WriteLine("\nSELECCIONE:");
-                Console.WriteLine("(1) PARA ORDEN ASCENDENTE");
-                Console.WriteLine("(2) PARA ORDEN DESCENDENTE");
-                eleccion = Console.ReadLine();
-
-                switch (eleccion)
-                {
-                    case "1":
-                        var ordenarEdad = estudiantes.OrderBy(e => e.Edad);
-                        Console.WriteLine($"\nOrdenar estudiantes por edad ascendente:");
-                        foreach (var item in ordenarEdad)
-                        {
-                            Console.WriteLine($"-{item.Nombre} de {item.Edad} años, estudiante de {item.Carrera}.");
-                        }
-                        break;
-                    case "2":
-                        ordenarEdad = estudiantes.OrderByDescending(e => e.Edad);
-                        Console.WriteLine($"\nOrdenar estudiantes por edad descendente:");
-                        foreach (var item in ordenarEdad)
-                        {
-                            Console.WriteLine($"-{item.Nombre} de {item.Edad} años, estudiante de {item.Carrera}.");
-                        }
-                        break;
-
-                    default:
-                        Console.WriteLine("Ha seleccionado una opción inválida!!!");
-                        break;
-                }
+                Console.WriteLine($"-{estudiante.Nombre} de {estudiante.Edad} años, estudiante de {estudiante.Carrera}.");
             }
+
+            //OrderByDescending, ordenamiento lógico (orden descendente)
+            var ordenarEdadDescendente = estudiantes.OrderByDescending(e => e.Edad);
+            Console.WriteLine($"\nOrdenar estudiantes por edad descendente:");
+            foreach (var estudiante in ordenarEdadDescendente)
+            {
+                Console.WriteLine($"-{estudiante.Nombre} de {estudiante.Edad} años, estudiante de {estudiante.Carrera}.");
+            }
+
 
 
 
