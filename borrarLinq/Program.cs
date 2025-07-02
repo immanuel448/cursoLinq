410a64b732c33d023c6c05250c2ae4963321242b
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-07-01 22:26:41 -0600

    001r

--- borrarLinq/Program.cs
+++ borrarLinq/Program.cs
@@ -1,9 +1,7 @@
 ﻿
-using System.Globalization;
-
 namespace  CursoLINQ
 {
-    public class Estudiantes
+    public class Estudiante
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -15,15 +13,14 @@ namespace  CursoLINQ
     {
         static void Main(string[] args)
         {
-            //hola
             //fuente de datos estática
-            List<Estudiantes> estudiantes = new List<Estudiantes>()
+            List<Estudiante> estudiantes = new List<Estudiante>()
             {
-                new Estudiantes{ Id = 1, Nombre = "Nadia", Edad = 19, Carrera ="Medicina" },
-                new Estudiantes{ Id = 2, Nombre = "Iker", Edad = 20, Carrera ="Ingeniería" },
-                new Estudiantes{ Id = 3, Nombre = "Armando", Edad = 18, Carrera ="Derecho" },
-                new Estudiantes{ Id = 4, Nombre = "Erika", Edad = 22, Carrera ="Arquitectura" },
-                new Estudiantes{ Id = 5, Nombre = "Oldair", Edad = 20, Carrera ="Ingeniería" }
+                new Estudiante{ Id = 1, Nombre = "Nadia", Edad = 19, Carrera ="Medicina" },
+                new Estudiante{ Id = 2, Nombre = "Iker", Edad = 20, Carrera ="Ingeniería" },
+                new Estudiante{ Id = 3, Nombre = "Armando", Edad = 18, Carrera ="Derecho" },
+                new Estudiante{ Id = 4, Nombre = "Erika", Edad = 22, Carrera ="Arquitectura" },
+                new Estudiante{ Id = 5, Nombre = "Oldair", Edad = 20, Carrera ="Ingeniería" }
             };
 
             //LINQ-----------------------
@@ -60,7 +57,7 @@ namespace  CursoLINQ
             //}
 
             // GROUPBY: Agrupa elementos que tienen una misma clave
-            //var conteoPorCarrera = estudiantes.GroupBy(e => e.Carrera == "Ingeniería");
+            //var conteoPorCarrera = estudiantes.GroupBy(e => e.Carrera);
             //Console.WriteLine("\nGROUPBY, Cantidad de estudiantes por carrera:");
             //foreach (var grupo in conteoPorCarrera)
             //{
@@ -69,24 +66,24 @@ namespace  CursoLINQ
 
             // ANY: Devuelve true si al menos un elemento cumple una condición
             //var ciertaEdad = estudiantes.Any(e => e.Edad > 18);
-            //Console.WriteLine($"\n Any, Saber si hay extudiantes mayores de 20 años:");
+            //Console.WriteLine($"\n Any, Saber si hay extudiantes mayores de 18 años:");
             //if (ciertaEdad)
             //{
-            //    Console.WriteLine($"Sí hay al menos un estudiante mayor de 20 años.");
+            //    Console.WriteLine($"Sí hay al menos un estudiante mayor de 18 años.");
             //}
             //else
             //{
-            //    Console.WriteLine($"No hay ningún estudiante mayor de 20 años.");
+            //    Console.WriteLine($"No hay ningún estudiante mayor de 18 años.");
             //}
 
-            // FIRSTORDEFAULT: Devuelve el primer elemento o un valor por defecto si no hay elementos
+            // ORDERBY - FIRSTORDEFAULT: Devuelve el primer elemento o un valor por defecto si no hay elementos
             //var primerEstudiante = estudiantes.OrderBy(e => e.Nombre).FirstOrDefault();
             //Console.WriteLine($"\n OrderBy y FirstOrDefault, nos devuelve el primer nombre ordenado alfabéticamente:");
             //Console.WriteLine($"El nombre del primer estudiante en orden alfabético es {primerEstudiante.Nombre}.");
 
             // AVERAGE: Calcula el promedio de los valores numéricos
             //var promedio = estudiantes.Average(e => e.Edad);
-            //Console.WriteLine($"\nA verage, Se obtiene el promedio de la edad:");
+            //Console.WriteLine($"\n Average, Se obtiene el promedio de la edad:");
             //Console.WriteLine($"El promedio de la edad de los estudientes es de {promedio} años.");
 
             // SELECT con objetos anónimos: Proyección personalizada de los datos
